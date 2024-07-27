245546d8e89e0e8c01ea6e420c535819ed0d2b0a
maple5717 <momiji5717@gmail.com>
2024-07-27 18:31:20 +0800

    launch

--- src/slam/wheelchair_slam/launch/orbslam_start.launch.py
+++ src/slam/wheelchair_slam/launch/orbslam_start.launch.py
@@ -5,26 +5,25 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    orbslam3_share_dir = FindPackageShare('orbslam3_ros2').find('orbslam3_ros2')
+    orbslam3_share_dir = FindPackageShare('orbslam3').find('orbslam3')
     wheelchair_slam_share_dir = FindPackageShare('wheelchair_slam').find('wheelchair_slam')
-    return LaunchDescription([
-        # Launch the RealSense2 camera node
+    voc_file =  str(orbslam3_share_dir) + '/vocabulary/ORBvoc.txt'
+    camera_file = str(wheelchair_slam_share_dir) + '/config/RealSense_D435i.yaml'
     
+    return LaunchDescription([
+
         # Launch the ORB-SLAM3 node
         Node(
             package='orbslam3',
-            executable='orbslam3_node',  # Update this with the actual executable name
+            executable='rgbd-inertial',  # Update this with the actual executable name
             name='orbslam3',
             output='screen',
             parameters=[
-                {'vocabulary_file': str(orbslam3_share_dir) + '/vocabulary/ORBvoc.txt'},
-                {'config_file': str(wheelchair_slam_share_dir) + 'config/RealSense_D435i.yaml'},
-                {'depth_module.depth_profile': '640x480x30'},
-                {'depth_module.infra_profile': '640x480x30'}, 
-                {'rgb_camera.color_profile': '1280x720x30'}
+
             ],
             remappings=[
                 # Add any required topic remappings here if necessary
             ],
+            arguments=[ voc_file, camera_file]
         ),
     ])
--- src/slam/wheelchair_slam/launch/realsense_start.launch.py
+++ src/slam/wheelchair_slam/launch/realsense_start.launch.py
@@ -1,28 +1,45 @@
-import launch
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from os.path import join
 
 def generate_launch_description():
-    # realsense2_share_dir = FindPackageShare('realsense2_camera').find('realsense2_camera')
+    realsense2_share_dir = FindPackageShare('realsense2_camera').find('realsense2_camera')
     
     return LaunchDescription([
         # Launch the RealSense2 camera node
-        Node(
-            package='realsense2_camera',
-            executable='realsense2_camera_node',
-            name='realsense2_camera',
-            output='screen',
-            parameters=[
-                {'unite_imu_method': 2},
-                {'align_depth.enable': True},
-                {'enable_accel': True},
-                {'enable_gyro': True},
-            ],
-            remappings=[
-                # Add any required topic remappings here if necessary
-            ],
+        # Node(
+        #     package='realsense2_camera',
+        #     executable='realsense2_camera_node',
+        #     name='realsense2_camera',
+        #     output='screen',
+        #     parameters=[
+        #         {'unite_imu_method': 2},
+        #         {'align_depth.enable': True},
+        #         {'enable_accel': True},
+        #         {'enable_gyro': True},
+        #         {'depth_module.depth_profile': '640x480x30'},
+        #         {'depth_module.infra_profile': '640x480x30'}, 
+        #         {'rgb_camera.color_profile': '1280x720x30'}
+        #     ],
+        #     remappings=[
+        #         # Add any required topic remappings here if necessary
+        #     ],
+        # ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(join(realsense2_share_dir, 'launch/rs_launch.py')),
+            launch_arguments={
+                'unite_imu_method': '2',
+                'align_depth.enable': 'true',
+                'enable_accel': 'true',
+                'enable_gyro': 'true',
+                'depth_module.depth_profile': '640x480x30',
+                # 'depth_module.infra_profile': '640x480x30', 
+                'rgb_camera.color_profile': '640x480x30'
+            }.items()
         ),
         
     
