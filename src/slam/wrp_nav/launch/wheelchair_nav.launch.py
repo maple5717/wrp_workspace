48b99667e5572af09ddac94f319d4aca007b18eb
maple5717 <momiji5717@gmail.com>
2024-11-11 16:52:57 +0800

    nav launch file

--- src/slam/wrp_nav/launch/wheelchair_nav.launch.py
+++ src/slam/wrp_nav/launch/wheelchair_nav.launch.py
@@ -2,31 +2,35 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetRemap
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     # Declare the arguments to the launch file
+    use_pid = DeclareLaunchArgument('use_pid', default_value='True', description='whether to inclued another PID loop outside the wheelchair controller')
 
     robot_nav_dir = FindPackageShare('wrp_nav').find('wrp_nav')
 
     # Path to the YAML config file in the robot_nav package
-    params_file_path = os.path.join(robot_nav_dir, 'config', 'nav2_params.yaml')
+    nav2_param_file_name = 'nav2_params.yaml' if use_pid else 'nav2_params_no_pid.yaml'
+    params_file_path = os.path.join(robot_nav_dir, 'config', nav2_param_file_name)
 
     base_launch_file_path = os.path.join(
-        get_package_share_directory('nav2_bringup'),
+        robot_nav_dir, # get_package_share_directory('nav2_bringup'),
         'launch',
         'navigation_launch.py'
     )
 
     return LaunchDescription([
+            # SetRemap(src='/cmd_vel',dst='/expected_vel'),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(base_launch_file_path),
                 launch_arguments={
                     'use_sim_time': 'false',
-                    'params_file': params_file_path
+                    'params_file': params_file_path, 
+                    # 'remappings': '["/cmd_vel:=/expected_vel"]',
                 }.items()
             ),
         ])
