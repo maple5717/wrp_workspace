a1cd3cd10dd8797777bdc2f9a053af3ef4286656
maple5717 <momiji5717@gmail.com>
2024-11-21 20:49:43 +0800

    launch nav2 withadditional  pid control

--- src/slam/wrp_nav/launch/wheelchair_nav.launch.py
+++ src/slam/wrp_nav/launch/wheelchair_nav.launch.py
@@ -11,26 +11,28 @@ def generate_launch_description():
     # Declare the arguments to the launch file
     use_pid = DeclareLaunchArgument('use_pid', default_value='True', description='whether to inclued another PID loop outside the wheelchair controller')
 
+    # ros2_nav_dir contains the nav2 launch file without any additional remapping
+    # robot_nav_dir contains the launch file that remaps cmv_vel to expected_vel
     robot_nav_dir = FindPackageShare('wrp_nav').find('wrp_nav')
+    ros2_nav_dir = FindPackageShare('nav2_bringup').find('wrp_nav')
+    launch_file_path = robot_nav_dir if use_pid else ros2_nav_dir
 
     # Path to the YAML config file in the robot_nav package
     nav2_param_file_name = 'nav2_params.yaml' if use_pid else 'nav2_params_no_pid.yaml'
     params_file_path = os.path.join(robot_nav_dir, 'config', nav2_param_file_name)
 
     base_launch_file_path = os.path.join(
-        robot_nav_dir, # get_package_share_directory('nav2_bringup'),
+        launch_file_path, # get_package_share_directory('nav2_bringup'),
         'launch',
         'navigation_launch.py'
     )
 
     return LaunchDescription([
-            # SetRemap(src='/cmd_vel',dst='/expected_vel'),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(base_launch_file_path),
                 launch_arguments={
                     'use_sim_time': 'false',
                     'params_file': params_file_path, 
-                    # 'remappings': '["/cmd_vel:=/expected_vel"]',
                 }.items()
             ),
         ])
